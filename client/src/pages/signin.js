4b88d6dfdd3b6366c09fb1e6886b11fc55fb0495
sumana basu <sumana.basu2001@gmail.com>
2021-05-04 13:28:34 +0530

    context

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -1,8 +1,10 @@
-import React, {useState} from "react";
+import React, {useState,useContext} from "react";
 import { Link,useHistory } from "react-router-dom";
 import M from 'materialize-css';
+import {UserContext} from '../App';
 
 const SignIn = () => {
+  const {state,dispatch}=useContext(UserContext)
   const history=useHistory()
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
@@ -25,6 +27,7 @@ const SignIn = () => {
       else{
         localStorage.setItem("jwt",data.token)
         localStorage.setItem("user",JSON.stringify(data.user))
+        dispatch({type:"USER",payload:data.user})
         M.toast({html:"Signed In", classes:"green darken-1"})
         history.push('/')
       }
