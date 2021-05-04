3bcea407926d0097c775162bf5dd63d86c9ac276
sumana basu <sumana.basu2001@gmail.com>
2021-05-04 13:38:28 +0530

    navlinks changed

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React,{useContext} from "react";
 import {Link} from 'react-router-dom';
+import {UserContext} from '../../App';
 
 const Navbar = () => {
+  const {state,dispatch}=useContext(UserContext)
+  const renderList=()=>{
+    if(state){
+      return [
+        <li><Link to="/profile">Profile</Link></li>,
+        <li><Link to="/createpost">Create Post</Link></li>
+      ]
+    }else{
+      return [
+        <li><Link to="/signin">Sign In</Link></li>,
+        <li><Link to="/signup">Sign Up</Link></li>
+      ]
+    }
+  }
   return (
     <div>
       <nav>
         <div className="nav-wrapper white">
-          <Link to="/" className="brand-logo left">
+          <Link to={state?"/":"/signin"} className="brand-logo left">
             Aliferous
           </Link>
           <ul id="nav-mobile" className="right">
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/createpost">Create Post</Link>
-            </li>
+            {renderList()}
           </ul>
         </div>
       </nav>
