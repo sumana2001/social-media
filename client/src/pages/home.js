cf67e922416aec1995524b49e1dd8457255f48ff
sumana basu <sumana.basu2001@gmail.com>
2021-05-05 10:11:14 +0530

    like unlike

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,42 +1,118 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../App";
 
 const Home = () => {
-  const [data,setData]=useState([])
-  useEffect(()=>{
-    fetch("/feed",{
-      headers:{
-        "Authorization":"Bearer "+localStorage.getItem("jwt")
-      }
-    }).then(res=>res.json())
-    .then(result=>{
-      setData(result.posts)
+  const { state, dispatch } = useContext(UserContext);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/feed", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
     })
-  },[])
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.posts);
+      });
+  }, []);
+
+  const likePost = (id) => {
+    fetch("/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const unlikePost = (id) => {
+    fetch("/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="home">
-      {
-        data.map(item=>{
-          return(
-            <div className="card home-card" key={item._id}>
-        <h5 style={{paddingLeft:"2%",paddingTop:"2%"}}>{item.postedBy.name}</h5>
-        <div className="card-image">
-          <img
-            src={item.photo}/>
-        </div>
-        <div className="card-content">
-          <i className="material-icons" style={{ color: "red" }}>
-            favorite
-          </i>
-          <h6>{item.title}</h6>
-          <p>{item.body}</p>
-          <input type="text" placeholder="Add a comment..." />
-        </div>
-      </div>
-          )
-        })
-      }
-      
-      </div>
+      {data.map((item) => {
+        return (
+          <div className="card home-card" key={item._id}>
+            <h5 style={{ paddingLeft: "2%", paddingTop: "2%" }}>
+              {item.postedBy.name}
+            </h5>
+            <div className="card-image">
+              <img src={item.photo} />
+            </div>
+            <div className="card-content">
+              {item.likes.includes(state._id) ? (
+                <i
+                  className="material-icons"
+                  style={{ cursor: "pointer", color: "red" }}
+                  onClick={() => {
+                    unlikePost(item._id);
+                  }}
+                >
+                  favorite
+                </i>
+              ) : (
+                <i
+                  className="material-icons"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    likePost(item._id);
+                  }}
+                >
+                  favorite_border
+                </i>
+              )}
+
+              <h6>{item.likes.length} likes</h6>
+              <h6>{item.title}</h6>
+              <p>{item.body}</p>
+              <input type="text" placeholder="Add a comment..." />
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,51 +1,89 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const requireLogin=require('../middleware/requireLogin')
-const Post=mongoose.model("Post");
+const requireLogin = require("../middleware/requireLogin");
+const Post = mongoose.model("Post");
 
-router.get('/feed',requireLogin,(req,res)=>{
-    Post.find()
-    .populate("postedBy","_id name")
-    .then(posts=>{
-        res.json({posts})
+router.get("/feed", requireLogin, (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id name")
+    .then((posts) => {
+      res.json({ posts });
     })
-    .catch(err=>{
-        console.log(err)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body,pic}=req.body
-    if(!title || !body ||!pic){
-        return res.status(422).json({error:"Please fill all the fields"})
+router.post("/createpost", requireLogin, (req, res) => {
+  const { title, body, pic } = req.body;
+  if (!title || !body || !pic) {
+    return res.status(422).json({ error: "Please fill all the fields" });
+  }
+
+  req.user.password = undefined;
+
+  const post = new Post({
+    title,
+    body,
+    photo: pic,
+    postedBy: req.user,
+  });
+  post
+    .save()
+    .then((result) => {
+      res.json({ post: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/myposts", requireLogin, (req, res) => {
+  Post.find({ postedBy: req.user._id })
+    .populate("postedBy", "_id name")
+    .then((mypost) => {
+      res.json({ mypost });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.put("/like", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { likes: req.user._id },
+    },
+    {
+      new: true,
     }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
 
-    req.user.password=undefined
-
-    const post=new Post({
-        title,
-        body,
-        photo:pic,
-        postedBy:req.user
-    })
-    post.save().then(result=>{
-        res.json({post:result})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-})
-
-router.get('/myposts',requireLogin,(req,res)=>{
-    Post.find({postedBy:req.user._id})
-    .populate("postedBy","_id name")
-    .then(mypost=>{
-        res.json({mypost})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-})
+router.put("/unlike", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $pull: { likes: req.user._id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 module.exports = router;
